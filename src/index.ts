6ae196faea592ef402be0825e447ab069d5840b7
Dan Dodd <dan@foredev.com>
2022-08-09 15:39:42 -0500

    rm the build dir

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 // eslint-disable-next-line no-console
 console.log('Hello world!');
+
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT ?? 8001;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
